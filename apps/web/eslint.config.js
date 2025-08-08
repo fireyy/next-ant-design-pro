57365ad85f15d18ebc491473fa677cb4764d920f
fireyy <fireyy@gmail.com>
2025-08-08 20:43:06 +0800

    fix: eslint config

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,10 @@
 import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { defineConfig } from "eslint/config";
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+export default defineConfig([
+  ...nextJsConfig,
+  {
+    ignores: ["./app/**/_mock.ts", "./app/**/data.d.ts"],
+  }
+]);
